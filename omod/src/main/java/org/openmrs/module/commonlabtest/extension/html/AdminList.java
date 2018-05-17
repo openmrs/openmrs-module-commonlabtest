e0c0f3405c0f7ca6ba6c472c078be0e5dc128357
owais.hussain <owais.hussain@ihsinformatics.com>
2018-05-17 11:30:43 +0500

    Changed JSP file names according to Openmrs conventions

--- omod/src/main/java/org/openmrs/module/commonlabtest/extension/html/AdminList.java
+++ omod/src/main/java/org/openmrs/module/commonlabtest/extension/html/AdminList.java
@@ -25,6 +25,7 @@ public class AdminList extends AdministrationSectionExt {
 	/**
 	 * @see org.openmrs.module.web.extension.AdministrationSectionExt#getMediaType()
 	 */
+	@Override
 	public Extension.MEDIA_TYPE getMediaType() {
 		return Extension.MEDIA_TYPE.html;
 	}
@@ -32,6 +33,7 @@ public class AdminList extends AdministrationSectionExt {
 	/**
 	 * @see org.openmrs.module.web.extension.AdministrationSectionExt#getTitle()
 	 */
+	@Override
 	public String getTitle() {
 		return "commonlabtest.title";
 	}
@@ -39,12 +41,12 @@ public class AdminList extends AdministrationSectionExt {
 	/**
 	 * @see org.openmrs.module.web.extension.AdministrationSectionExt#getLinks()
 	 */
+	@Override
 	public Map<String, String> getLinks() {
 		
 		Map<String, String> map = new HashMap<String, String>();
-		map.put("/module/commonlabtest/commonlabtest.form", "commonlabtest.title");
-		map.put("/module/commonlabtest/pages/managetesttypes.form", "commonlabtest.admin.managetesttypes");
-		map.put("/module/commonlabtest/pages/managetestattributetypes.form", "commonlabtest.admin.managetestattributetypes");
+		map.put("/module/commonlabtest/pages/manageTestTypes.form", "commonlabtest.admin.managetesttypes");
+		map.put("/module/commonlabtest/pages/manageTestAttributeTypes.form", "commonlabtest.admin.managetestattributetypes");
 		return map;
 	}
 	
